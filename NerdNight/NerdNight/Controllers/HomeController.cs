1e71c2a196b9c5394d1455a913dd9dfc6560f98f
BudPrager <silver.fang@hotmail.co.uk>
2017-11-05 19:38:35 +0000

    Change gitignore to an external config file so that app should run straight away after a clone

--- NerdNight/NerdNight/Controllers/HomeController.cs
+++ NerdNight/NerdNight/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,7 @@ namespace NerdNight.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "Secret key = " + ConfigurationManager.AppSettings["SecretKey"];
             return View();
         }
 
